28d84a22fbc10fa40f16ae339ffd159153320bbc
Florinda Hasani <florinda.hasani@valtech.com>
2025-04-07 11:31:06 +0200

    search bar

---
+++ app/src/main/java/com/example/rickandmorty/viewmodels/SearchViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.rickandmorty.viewmodels
+
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.runtime.snapshotFlow
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor() : ViewModel() {
+    val searchTextFieldState = TextFieldState()
+
+    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    val searchTextState = snapshotFlow {
+        searchTextFieldState.text
+    }.debounce(500)
+        .mapLatest { if (it.isBlank()) "Awaiting your command..." else it.toString() }
+        .stateIn(
+            initialValue = "",
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 2000)
+        )
+
+}
